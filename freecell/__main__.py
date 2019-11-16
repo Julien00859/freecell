087ce380f47452b62add6be6b340fc0053dbd2ac
Julien Castiaux <Julien.castiaux@gmail.com>
2019-11-16 21:48:19 +0100

    cli option to set game seed

--- freecell/__main__.py
+++ freecell/__main__.py
@@ -1,6 +1,15 @@
+import random
 import sys
+from argparse import ArgumentParser
 from curses import wrapper
 from freecell.tui import main
 from freecell.game import Game
 
-wrapper(main, Game(), debug='--debug' in sys.argv)
+parser = ArgumentParser()
+parser.add_argument('--debug', action='store_true', default=False)
+parser.add_argument('--seed', action='store', type=int, default=None)
+options = parser.parse_args()
+if options.seed is not None:
+    random.seed(options.seed)
+
+wrapper(main, Game(), debug=options.debug)
