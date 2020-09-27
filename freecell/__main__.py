0a1195f15f96b029a989e0b1add13bf5306da4c8
Julien Castiaux <julien.castiaux@gmail.com>
2020-09-28 00:14:01 +0200

    yeah gui !! (no)

--- freecell/__main__.py
+++ freecell/__main__.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import random
-import sys
 from argparse import ArgumentParser
 from base64 import b64encode, b64decode
-from curses import wrapper
-from freecell.tui import main
-from freecell.game import Game
+from functools import partial
+from os import getenv
 from time import time
+from freecell.game import Game
 
 parser = ArgumentParser()
+parser.add_argument("display", choices=["TUI", "GUI"])
 parser.add_argument("--debug", action="store_true", default=False)
 parser.add_argument("--seed", action="store", type=int, default=None)
 parser.add_argument("--restore", action="store")
 options = parser.parse_args()
+
+# Initialise backend
 seed = options.seed or int(time())
 random.seed(seed)
 print("Game seed:", seed)
@@ -22,6 +24,15 @@ if options.restore:
 else:
     game = Game()
 
-wrapper(main, game, debug=options.debug)
+# Select frontend
+if options.display == "GUI":
+    from freecell.gui import main
+else:
+    from curses import wrapper
+    from freecell.tui import main
+    main = partial(wrapper, main)
+
+# Start the game
+main(game, debug=options.debug)
 if not game.won:
     print("Freeze:", b64encode(game.freeze()).decode())
